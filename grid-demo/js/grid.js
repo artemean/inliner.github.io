a51375c98b694cf06dd5d36ee00bc04037fc7ffe
Andriy Artemenko <Andriy_Artemenko@epam.com>
2016-04-27 17:33:11 +0300

    grid-demo, filtering reorganized

--- grid-demo/js/grid.js
+++ grid-demo/js/grid.js
@@ -5,29 +5,31 @@ $(document).ready(function() {
         toolbar: ["excel"],
         excel: {
             fileName: "Kendo UI Grid Export.xlsx",
-            //proxyURL: "//demos.telerik.com/kendo-ui/service/export",
+            proxyURL: "//demos.telerik.com/kendo-ui/service/export",
             filterable: true
         },
         dataSource: {
 //                    data: products,
 
             transport: {
-                read: "data/data2.json",
+                read: "data/data5000.json",
                 type: "json"
             },
             schema: {
                 model: {
                     fields: {
                         Issuer: { type: "string" },
-                        Tranche: { type: "string" },
-                        Broker_Alpha: { type: "string" },
-                        Pricing_Date: { type: "string" },
-                        Transaction_Type: { type: "string" },
                         Product_Type: { type: "string" },
+                        Tranche: { type: "string" },
+                        Pricing_Date: { type: "date" },
                         Indication: { type: "number" },
                         Allocation: { type: "number" },
                         Percent_allocation: { type: "string" },
-                        Some: { type: "number" },
+                        Hedge_Type: { type: "string" },
+                        Security_Type: { type: "string" },
+                        Retention_Allocation: { type: "number" },
+                        Sales_Person: { type: "string" },
+                        Region: { type: "string" }
                     }
                 }
             },
@@ -40,6 +42,10 @@ $(document).ready(function() {
             //aggregate: [ { field: "Issuer", aggregate: "count" },
             //    { field: "Indication", aggregate: "min" },
             //    { field: "Some", aggregate: "sum" }],
+            sort: [{
+                field: "Pricing_Date",
+                dir: "desc"
+            }],
             pageSize: 100
         },
 
@@ -52,7 +58,7 @@ $(document).ready(function() {
         groupable: true,
         resizable: true,
         filterable: true,
-        columnMenu: true,
+        //columnMenu: true,
 //                pageable: {
 //                    input: true,
 //                    numeric: false
@@ -60,46 +66,33 @@ $(document).ready(function() {
         columns: [
             {
                 field: "Issuer",
-                title: "Issuer",
+                title: "Issuer"
                 //footerTemplate: "Total Count: #=count#",
                 //locked: true,
                 //lockable: true,
-                filterable: { multi: true }
+                //filterable: { multi: true }
                 //,width: 150
+            },
+            {
+                field: "Product_Type",
+                title: "Product Type",
+                filterable: { multi: true }
             },
             {
                 field: "Tranche",
-                title: "Tranche",
-                filterable: { multi: true }
-                //locked: false,
-                //lockable: true
-                //,width: 150
-            },
-            {
-                field: "Broker_Alpha",
-                title: "Broker Alpha"
+                title: "Tranche"
+                //filterable: { multi: true }
                 //locked: false,
                 //lockable: true
                 //,width: 150
             },
             {
                 field: "Pricing_Date",
-                title: "Pricing Date"
-                //locked: false,
-                //lockable: true
-                //,width: 150
-            },
-
-            {
-                field: "Transaction_Type",
-                title: "Transaction Type"
-                //locked: false,
-                //lockable: true
-                //,width: 150
-            },
-            {
-                field: "Product_Type",
-                title: "Product Type"
+                title: "Pricing Date",
+                format: "{0:dd/MMM/yy}",
+                filterable: {
+                    ui: "datetimepicker" // use Kendo UI DateTimePicker
+                }
                 //locked: false,
                 //lockable: true
                 //,width: 150
@@ -122,17 +115,49 @@ $(document).ready(function() {
             },
             {
                 field: "Percent_allocation",
-                title: "Percent allocation"
+                title: "% Allocation"
                 //,width: 150
             },
             {
-                field: "Some",
-                title: "Some",
-                format: "{0:c}"
-                //aggregates: ["sum"],
-                //footerTemplate: "Sum: #= kendo.toString(sum, 'c') #"
-                //,width: 150
+                field: "Hedge_Type",
+                title: "Hedge Type",
+                filterable: { multi: true }
+            },
+            {
+                field: "Security_Type",
+                title: "Security Type",
+                filterable: { multi: true }
+            },
+            {
+                field: "Retention_Allocation",
+                title: "Retention  Allocation",
+                format: "{0:c0}"
+            },
+            {
+                field: "Sales_Person",
+                title: "Sales Person"
+            },
+            {
+                field: "Region",
+                title: "Region",
+                filterable: { multi: true }
             }
+
+
+
+
+            //{
+            //    field: "Some",
+            //    title: "Some",
+            //    format: "{0:c}"
+            //    //aggregates: ["sum"],
+            //    //footerTemplate: "Sum: #= kendo.toString(sum, 'c') #"
+            //    //,width: 150
+            //}
+            //{
+            //    field: "Transaction_Type",
+            //    title: "Transaction Type"
+            //},
         ]
     });
 
@@ -141,6 +166,7 @@ $(document).ready(function() {
     $("#save").click(function (e) {
         e.preventDefault();
         localStorage["kendo-grid-options"] = kendo.stringify(gridData.getOptions());
+        console.log(gridData.getOptions());
     });
 
     $("#load").click(function (e) {
