7a34d020b1f8e43b3bbfb5036e70e5bdd42b990b
Andriy Artemenko <Andriy_Artemenko@epam.com>
2016-05-12 14:54:44 +0300

    ajax

---
+++ grid-demo/js/columnSettings.js
@@ -0,0 +1,135 @@
+var columnSettings = {
+    allocationPercentColumnTitle: "% Allocation",
+    pricingDateColumnTitle: "Pricing Date",
+    columns: [
+        {
+            field: "issue",
+            title: "Issue",
+            filterable: {
+                extra: false,
+                operators: {
+                    string: {
+                        contains: "Contains"
+                    }
+                }
+            }
+        },
+        {
+            field: "productType",
+            title: "Product Type",
+            filterable: { multi: true }
+        },
+        {
+            field: "tranche",
+            title: "Tranche",
+            filterable: {
+                extra: false,
+                operators: {
+                    string: {
+                        contains: "Contains"
+                    }
+                }
+            }
+        },
+        {
+            field: "pricingDate",
+            title: this.pricingDateColumnTitle,
+            format: "{0:yyyy-MM-dd}",
+            filterable: {
+                ui: "datepicker", // use Kendo UI DateTimePicker
+                operators: {
+                    date: {
+                        gte: "After",
+                        lte: "Before"
+                    }
+                }
+            }
+        },
+        {
+            field: "indication",
+            title: "Indication",
+            format: "{0:n}",
+            filterable: {
+                operators: {
+                    number: {
+                        gte: "Greater or equal",
+                        lte: "Less or equal"
+                    }
+                }
+            }
+        },
+        {
+            field: "allocation",
+            title: "Allocation",
+            format: "{0:n}",
+            filterable: {
+                operators: {
+                    number: {
+                        gte: "Greater or equal",
+                        lte: "Less or equal"
+                    }
+                }
+            }
+        },
+        {
+            field: "allocationPercentage",
+            title: this.allocationPercentColumnTitle,
+            template: function (dataItem) {
+                var val = dataItem["allocationPercentage"];
+                if (val !== undefined) {
+                    if (val === null) {
+                        return "";
+                    }
+                    return kendo.toString(val, "n2") + "%";
+                } else {
+                    return "";
+                }
+            },
+            filterable: {
+                operators: {
+                    number: {
+                        gte: "Greater or equal",
+                        lte: "Less or equal"
+                    }
+                }
+            }
+        },
+        {
+            field: "retentionAllocation",
+            title: "Retention  Allocation",
+            format: "{0:n}",
+            filterable: {
+                operators: {
+                    number: {
+                        gte: "Greater or equal",
+                        lte: "Less or equal"
+                    }
+                }
+            }
+        },
+        {
+            field: "salesPerson",
+            title: "Sales Person",
+            filterable: {
+                extra: false,
+                operators: {
+                    string: {
+                        contains: "Contains"
+                    }
+                }
+            }
+        },
+        {
+            field: "region",
+            title: "Region",
+            filterable: {
+                extra: false,
+                operators: {
+                    string: {
+                        contains: "Contains"
+                    }
+                }
+            }
+        }
+    ]
+};
--- grid-demo/js/grid.js
+++ grid-demo/js/grid.js
@@ -1,240 +1,156 @@
 $(document).ready(function() {
-    var grid = $("#grid");
 
-    grid.kendoGrid({
-        toolbar: ["excel"],
-        excel: {
-            fileName: "Kendo UI Grid Export.xlsx",
-            proxyURL: "//demos.telerik.com/kendo-ui/service/export",
-            filterable: true
-        },
-        dataSource: {
-//                    data: products,
+    var grid = $("#grid"),
+        inverstorId = document.getElementById("investorId").value,
+        inverstorType = document.getElementById("investorType").value,
+        inverstorName = document.getElementById("investorName").value,
+        gridData,
+        columnsSet;
+        //columnsSet = JSON.parse(localStorage["kendo-grid-options"]);
 
-            transport: {
-                read: "data/data500.json",
-                type: "json"
-            },
-            schema: {
-                model: {
-                    fields: {
-                        Issue: { type: "string" },
-                        Product_Type: { type: "string" },
-                        Tranche: { type: "string" },
-                        Pricing_Date: { type: "date" },
-                        Indication: { type: "number" },
-                        Allocation: { type: "number" },
-                        Percent_allocation: { type: "number" },
-                        Hedge_Type: { type: "string" },
-                        Security_Type: { type: "string" },
-                        Retention_Allocation: { type: "number" },
-                        Sales_Person: { type: "string" },
-                        Region: { type: "string" }
-                    }
-                }
-            },
-            //        group: {
-            //            field: "Product_Type", aggregates: [
-            //                { field: "Indication", aggregate: "sum" },
-            //                { field: "Allocation", aggregate: "sum"}
-            //            ]
-            //        },
-            //aggregate: [ { field: "Issuer", aggregate: "count" },
-            //    { field: "Indication", aggregate: "min" },
-            //    { field: "Some", aggregate: "sum" }],
-            sort: [{
-                field: "Pricing_Date",
-                dir: "desc"
-            }],
-            pageSize: 100
-        },
-
-        height: 550,
-        scrollable: {
-            virtual: true
-        },
-        sortable: true,
-        reorderable: true,
-        groupable: true,
-        resizable: true,
-        filterable: {
-            //extra: false
-        },
-        columnMenu: {
-            sortable: false
-        },
-//                pageable: {
-//                    input: true,
-//                    numeric: false
-//                },
-        columns: [
-            {
-                field: "Issue",
-                title: "Issue",
-                //footerTemplate: "Total Count: #=count#",
-                //locked: true,
-                //lockable: true,
-                filterable: {
-                    extra: false,
-                    operators: {
-                        string: {
-                            contains: "Contains"
-                        }
-                    }
-                }
-            },
-            {
-                field: "Product_Type",
-                title: "Product Type",
-                filterable: { multi: true }
-            },
-            {
-                field: "Tranche",
-                title: "Tranche",
-                filterable: {
-                    extra: false,
-                    operators: {
-                        string: {
-                            contains: "Contains"
-                        }
-                    }
-                }
-            },
-            {
-                field: "Pricing_Date",
-                title: "Pricing Date",
-                format: "{0:dd/MMM/yy}",
-                filterable: {
-                    ui: "datepicker", // use Kendo UI DateTimePicker
-                    operators: {
-                        date: {
-                            gte: "After",
-                            lte: "Before"
-                        }
-                    }
-                }
-                //locked: false,
-                //lockable: true
-                //,width: 150
-            },
-            {
-                field: "Indication",
-                title: "Indication",
-                format: "{0:c0}",
-                filterable: {
-                    operators: {
-                        number: {
-                            gte: "Greater or equal",
-                            lte: "Less or equal"
-                        }
-                    }
-                }
-                //aggregates: ["min"],
-                //footerTemplate: "Min: #= kendo.toString(min, 'c') #",
-                //groupFooterTemplate: "Sum: #=sum#"
-                //,width: 150
-            },
-            {
-                field: "Allocation",
-                title: "Allocation",
-                format: "{0:c0}",
-                filterable: {
-                    operators: {
-                        number: {
-                            gte: "Greater or equal",
-                            lte: "Less or equal"
-                        }
-                    }
-                }
-                //groupFooterTemplate: "Sum: #= kendo.toString(sum, 'c') #"
-                //,width: 150
-            },
-            {
-                field: "Percent_allocation",
-                title: "% Allocation",
-                format: "{0:p}",
-                filterable: {
-                    operators: {
-                        number: {
-                            gte: "Greater or equal",
-                            lte: "Less or equal"
-                        }
-                    }
-                }
-            },
-            {
-                field: "Hedge_Type",
-                title: "Hedge Type",
-                filterable: { multi: true }
-            },
-            {
-                field: "Retention_Allocation",
-                title: "Retention  Allocation",
-                format: "{0:c0}",
-                filterable: {
-                    operators: {
-                        number: {
-                            gte: "Greater or equal",
-                            lte: "Less or equal"
-                        }
-                    }
-                }
-            },
-            {
-                field: "Sales_Person",
-                title: "Sales Person",
-                filterable: {
-                    extra: false,
-                    operators: {
-                        string: {
-                            contains: "Contains"
-                        }
-                    }
-                }
-            },
-            {
-                field: "Region",
-                title: "Region",
-                filterable: {
-                    extra: false,
-                    operators: {
-                        string: {
-                            contains: "Contains"
-                        }
-                    }
-                }
-            }
-            //{
-            //    field: "Some",
-            //    title: "Some",
-            //    format: "{0:c}",
-            //    columnMenu: true
-            //    //aggregates: ["sum"],
-            //    //footerTemplate: "Sum: #= kendo.toString(sum, 'c') #"
-            //    //,width: 150
-            //}
-            //{
-            //    field: "Transaction_Type",
-            //    title: "Transaction Type"
-            //},
-        ]
+    $(window).resize(function () {
+        grid.data("kendoGrid").resize();
     });
 
-    var gridData = grid.data("kendoGrid");
+    $.ajax({
+        url: 'http://api.mlab.com/api/1/databases/userlayout/collections/defaultLayout?apiKey=C96sHyGy2r5sSf-ibDakmsV5ypO-ea1Y',
+        type: 'GET',
+        contentType: "application/json",
+        success: function() { alert('hello!'); },
+        error: function() { alert('boo!'); }
+        //beforeSend: setHeader
+    });
 
-    $("#save").click(function (e) {
-        e.preventDefault();
-        localStorage["kendo-grid-options"] = kendo.stringify(gridData.getOptions());
+
+    function setHeader(xhr) {
+        xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+    }
+
+    //$.getJSON( "http://api.mlab.com/api/1/databases/userlayout/collections/defaultLayout?apiKey=C96sHyGy2r5sSf-ibDakmsV5ypO-ea1Y&callback=?")
+    //    .fail(function() {
+    //        console.log( "error" );
+    //    })
+    //    .done(function( data ) {
+    //    columnsSet = data.columns;
+    //    console.log(columnsSet);
+    //
+    //    grid.kendoGrid({
+    //        toolbar: ["excel"],
+    //        excelExport: function (e) {
+    //            var sheet = e.workbook.sheets[0];
+    //            var allocationPercentColumnIndex;
+    //            var pricingDateColumnIndex;
+    //
+    //            var headerRow = sheet.rows[0];
+    //
+    //            for (var i = 0; i < sheet.columns.length; i++) {
+    //                if (headerRow.cells[i].value === columnSettings.allocationPercentColumnTitle) {
+    //                    allocationPercentColumnIndex = i;
+    //                }
+    //                else if (headerRow.cells[i].value === columnSettings.pricingDateColumnTitle) {
+    //                    pricingDateColumnIndex = i;
+    //                }
+    //            }
+    //
+    //            for (var rowIndex = 1; rowIndex < sheet.rows.length; rowIndex++) {
+    //                var row = sheet.rows[rowIndex];
+    //                for (var cellIndex = 0; cellIndex < row.cells.length; cellIndex++) {
+    //                    if (rowIndex % 2 === 0) {
+    //                        row.cells[cellIndex].background = "#d5d5d5";
+    //                    }
+    //                    if (allocationPercentColumnIndex === cellIndex) {
+    //                        var val = row.cells[cellIndex].value;
+    //                        if (val !== undefined && val !== null) {
+    //                            row.cells[cellIndex].value = val / 100;
+    //                        }
+    //                        row.cells[cellIndex].format = "0.00%";
+    //                    }
+    //                    else if (pricingDateColumnIndex === cellIndex) {
+    //                        row.cells[cellIndex].format = "yyyy-MM-dd";
+    //                    }
+    //                }
+    //            }
+    //        },
+    //        excel: {
+    //            fileName: inverstorName + ".xlsx",
+    //            filterable: true,
+    //            allPages: true
+    //        },
+    //        dataSource: {
+    //            transport: {
+    //                //read: "../../web.api/InvestorHistory/" + inverstorId + '?investorType=' + inverstorType,
+    //                read: "data/data500.json",
+    //                type: "json"
+    //            },
+    //            schema: {
+    //                model: {
+    //                    fields: {
+    //                        issue: { type: "string" },
+    //                        productType: { type: "string" },
+    //                        tranche: { type: "string" },
+    //                        pricingDate: { type: "date" },
+    //                        indication: { type: "number" },
+    //                        allocation: { type: "number" },
+    //                        allocationPercentage: { type: "number" },
+    //                        retentionAllocation: { type: "number" },
+    //                        salesPerson: { type: "string" },
+    //                        region: { type: "string" }
+    //                    }
+    //                }
+    //            },
+    //            sort: [{
+    //                field: "pricingDate",
+    //                dir: "desc"
+    //            }],
+    //            pageSize: 100
+    //        },
+    //        scrollable: {
+    //            virtual: true
+    //        },
+    //        sortable: true,
+    //        reorderable: true,
+    //        groupable: true,
+    //        resizable: true,
+    //        filterable: true,
+    //        columnMenu: {
+    //            sortable: false
+    //        },
+    //        //columns: columnsSet || columnSettings.columns
+    //        columns: columnsSet
+    //    });
+    //});
+
+
+
+
+    gridData = grid.data("kendoGrid");
+
+    $("#save").click(function (event) {
+        event.preventDefault();
+        localStorage["kendo-grid-options"] = kendo.stringify(gridData.getOptions().columns);
         console.log(gridData.getOptions());
     });
 
-    $("#load").click(function (e) {
-        e.preventDefault();
-        var options = localStorage["kendo-grid-options"];
+    $("#load").click(function (event) {
+        event.preventDefault();
+        columnsSet = JSON.parse(localStorage["kendo-grid-options"]);
+        var options = gridData.getOptions();
+        options.columns = columnsSet;
         if (options) {
-            gridData.setOptions(JSON.parse(options));
+            gridData.setOptions(options);
         }
     });
+
+    $("#loadDefault").click(function (event) {
+        event.preventDefault();
+        //var columnsSettings = JSON.parse(localStorage["kendo-grid-options"]);
+        var options = gridData.getOptions();
+        options.columns = columnSettings.columns;
+        if (options) {
+            gridData.setOptions(options);
+        }
+    });
+
 
 
 
