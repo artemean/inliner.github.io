8cd70e64ded46c2326e4c800e1460cc7e972ed69
Andriy Artemenko <Andriy_Artemenko@epam.com>
2016-05-12 17:09:12 +0300

    fixed2

--- grid-demo/js/grid.js
+++ grid-demo/js/grid.js
@@ -40,132 +40,134 @@ $(document).ready(function() {
 
         columnsSet = data[0].columns;
 
-        grid.kendoGrid({
-            toolbar: ["excel"],
-            excelExport: function (e) {
-                var sheet = e.workbook.sheets[0];
-                var allocationPercentColumnIndex;
-                var pricingDateColumnIndex;
 
-                var headerRow = sheet.rows[0];
 
-                for (var i = 0; i < sheet.columns.length; i++) {
-                    if (headerRow.cells[i].value === columnSettings.allocationPercentColumnTitle) {
-                        allocationPercentColumnIndex = i;
+
+    });
+
+    grid.kendoGrid({
+        toolbar: ["excel"],
+        excelExport: function (e) {
+            var sheet = e.workbook.sheets[0];
+            var allocationPercentColumnIndex;
+            var pricingDateColumnIndex;
+
+            var headerRow = sheet.rows[0];
+
+            for (var i = 0; i < sheet.columns.length; i++) {
+                if (headerRow.cells[i].value === columnSettings.allocationPercentColumnTitle) {
+                    allocationPercentColumnIndex = i;
+                }
+                else if (headerRow.cells[i].value === columnSettings.pricingDateColumnTitle) {
+                    pricingDateColumnIndex = i;
+                }
+            }
+
+            for (var rowIndex = 1; rowIndex < sheet.rows.length; rowIndex++) {
+                var row = sheet.rows[rowIndex];
+                for (var cellIndex = 0; cellIndex < row.cells.length; cellIndex++) {
+                    if (rowIndex % 2 === 0) {
+                        row.cells[cellIndex].background = "#d5d5d5";
                     }
-                    else if (headerRow.cells[i].value === columnSettings.pricingDateColumnTitle) {
-                        pricingDateColumnIndex = i;
+                    if (allocationPercentColumnIndex === cellIndex) {
+                        var val = row.cells[cellIndex].value;
+                        if (val !== undefined && val !== null) {
+                            row.cells[cellIndex].value = val / 100;
+                        }
+                        row.cells[cellIndex].format = "0.00%";
+                    }
+                    else if (pricingDateColumnIndex === cellIndex) {
+                        row.cells[cellIndex].format = "yyyy-MM-dd";
                     }
                 }
-
-                for (var rowIndex = 1; rowIndex < sheet.rows.length; rowIndex++) {
-                    var row = sheet.rows[rowIndex];
-                    for (var cellIndex = 0; cellIndex < row.cells.length; cellIndex++) {
-                        if (rowIndex % 2 === 0) {
-                            row.cells[cellIndex].background = "#d5d5d5";
-                        }
-                        if (allocationPercentColumnIndex === cellIndex) {
-                            var val = row.cells[cellIndex].value;
-                            if (val !== undefined && val !== null) {
-                                row.cells[cellIndex].value = val / 100;
-                            }
-                            row.cells[cellIndex].format = "0.00%";
-                        }
-                        else if (pricingDateColumnIndex === cellIndex) {
-                            row.cells[cellIndex].format = "yyyy-MM-dd";
-                        }
-                    }
-                }
-            },
-            excel: {
-                fileName: inverstorName + ".xlsx",
-                filterable: true,
-                allPages: true
-            },
-            dataSource: {
-                transport: {
-                    //read: "../../web.api/InvestorHistory/" + inverstorId + '?investorType=' + inverstorType,
-                    read: "data/data500.json",
-                    type: "json"
-                },
-                schema: {
-                    model: {
-                        fields: {
-                            issue: { type: "string" },
-                            productType: { type: "string" },
-                            tranche: { type: "string" },
-                            pricingDate: { type: "date" },
-                            indication: { type: "number" },
-                            allocation: { type: "number" },
-                            allocationPercentage: { type: "number" },
-                            retentionAllocation: { type: "number" },
-                            salesPerson: { type: "string" },
-                            region: { type: "string" }
-                        }
-                    }
-                },
-                sort: [{
-                    field: "pricingDate",
-                    dir: "desc"
-                }],
-                pageSize: 100
-            },
-            scrollable: {
-                virtual: true
-            },
-            sortable: true,
-            reorderable: true,
-            groupable: true,
-            resizable: true,
+            }
+        },
+        excel: {
+            fileName: inverstorName + ".xlsx",
             filterable: true,
-            columnMenu: {
-                sortable: false
+            allPages: true
+        },
+        dataSource: {
+            transport: {
+                //read: "../../web.api/InvestorHistory/" + inverstorId + '?investorType=' + inverstorType,
+                read: "data/data500.json",
+                type: "json"
             },
-            //columns: columnsSet || columnSettings.columns
-            columns: columnsSet
-        });
-
-            gridData = grid.data("kendoGrid");
-
-            var dSource = gridData.dataSource;
-            console.log(dSource);
-            $.each(dSource.options.schema.model.fields, function (propertyName, propertyValue) {
-                //alert(filter.field);
-                console.log(propertyName, propertyValue);
-            });
-
-
-
-
-
-
-            $("#save").click(function (event) {
-                event.preventDefault();
-                localStorage["kendo-grid-options"] = kendo.stringify(gridData.getOptions().columns);
-                console.log(gridData.getOptions());
-            });
-
-            $("#load").click(function (event) {
-                event.preventDefault();
-                columnsSet = JSON.parse(localStorage["kendo-grid-options"]);
-                var options = gridData.getOptions();
-                options.columns = columnsSet;
-                if (options) {
-                    gridData.setOptions(options);
+            schema: {
+                model: {
+                    fields: {
+                        issue: { type: "string" },
+                        productType: { type: "string" },
+                        tranche: { type: "string" },
+                        pricingDate: { type: "date" },
+                        indication: { type: "number" },
+                        allocation: { type: "number" },
+                        allocationPercentage: { type: "number" },
+                        retentionAllocation: { type: "number" },
+                        salesPerson: { type: "string" },
+                        region: { type: "string" }
+                    }
                 }
-            });
+            },
+            sort: [{
+                field: "pricingDate",
+                dir: "desc"
+            }],
+            pageSize: 100
+        },
+        scrollable: {
+            virtual: true
+        },
+        sortable: true,
+        reorderable: true,
+        groupable: true,
+        resizable: true,
+        filterable: true,
+        columnMenu: {
+            sortable: false
+        },
+        //columns: columnsSet || columnSettings.columns
+        columns: columnsSet
+    });
 
-            $("#loadDefault").click(function (event) {
-                event.preventDefault();
-                //var columnsSettings = JSON.parse(localStorage["kendo-grid-options"]);
-                var options = gridData.getOptions();
-                options.columns = columnSettings.columns;
-                if (options) {
-                    gridData.setOptions(options);
-                }
-            });
+    gridData = grid.data("kendoGrid");
+
+    var dSource = gridData.dataSource;
+    console.log(dSource);
+    $.each(dSource.options.schema.model.fields, function (propertyName, propertyValue) {
+        //alert(filter.field);
+        console.log(propertyName, propertyValue);
+    });
 
 
+
+
+
+
+    $("#save").click(function (event) {
+        event.preventDefault();
+        localStorage["kendo-grid-options"] = kendo.stringify(gridData.getOptions().columns);
+        console.log(gridData.getOptions());
+    });
+
+    $("#load").click(function (event) {
+        event.preventDefault();
+        columnsSet = JSON.parse(localStorage["kendo-grid-options"]);
+        var options = gridData.getOptions();
+        options.columns = columnsSet;
+        if (options) {
+            gridData.setOptions(options);
+        }
+    });
+
+    $("#loadDefault").click(function (event) {
+        event.preventDefault();
+        //var columnsSettings = JSON.parse(localStorage["kendo-grid-options"]);
+        var options = gridData.getOptions();
+        options.columns = columnSettings.columns;
+        if (options) {
+            gridData.setOptions(options);
+        }
     });
 
 
